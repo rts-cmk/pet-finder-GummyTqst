f9897b1674c960874dac5d50098f45a99950ad0e
Tobias <neumsnn.tobias@gmail.com>
2025-11-21 09:39:56 +0100

    Added styling to the detail page

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,9 +1,13 @@
 import { useLoaderData, useNavigate } from "react-router"
+import BackgroundSvg from "../Assets/back.svg"
+import "../Styles/Details.sass"
 
 export default function Details( dog ) {
 
-    const pet = useLoaderData()
-    const { id, breed, location, short_description, image } = dog;
+    const petDetails = useLoaderData()
+
+    // console.log(petDetails)
+    // const { id, breed, location, short_description, image } = dog;
 
     const navigate = useNavigate();
 
@@ -11,17 +15,43 @@ export default function Details( dog ) {
         navigate(-1);
     }
 
-    if (!pet) {
+    if (!petDetails) {
         return <p>Pet not found</p>;
     }
 
     return (
-        <>
-            <h1>Details Page</h1>
+        <div className="details">
+            
+            {/* 1. The Header Area (Curved SVG Background) */}
+            <div className="details__header">
+                <img src={BackgroundSvg} alt="" className="details__svg-background" />
+                
+                {/* The main image, positioned to float over the content card */}
+                <img className="details__image" src={petDetails.image} alt={`A ${petDetails.breed}`} />
+            </div>
 
-            <button onClick={handleBack}>
-                Go back
-            </button>
-        </>
+            {/* 2. The Content Card (White, overlaps the curve) */}
+            <div className="details__content">
+                <div className="details__info">
+                    <h1 className="details__title">
+                        {petDetails.breed}
+                    </h1>
+                    <p className="details__location">
+                        {petDetails.location}
+                    </p>
+                </div>
+                
+                <p className="details__description">
+                    {petDetails.short_description}
+                </p>
+
+                <button 
+                    className="details__button"
+                    onClick={handleBack}
+                >
+                    Go back
+                </button>
+            </div>
+        </div>
     )
 }
